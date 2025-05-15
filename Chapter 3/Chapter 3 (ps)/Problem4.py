983f5e248476e1c28750695b78d1085baa895d3d
hiren223 <123714322+hiren223@users.noreply.github.com>
2025-05-15 10:14:18 +0530

    Done

---
+++ Chapter 3/Chapter 3 (ps)/Problem4.py
@@ -0,0 +1,5 @@
+# Replace the double Spaces from Problem 3 with Single spaces
+
+name = "This is  a  string with  double spaces"
+print(name.replace("  "," "))
+print(name)# String is immutable which means that you cannot change them by running function on them.
---
+++ Chapter 3/Chapter 3 (ps)/Problem5.py
@@ -0,0 +1,5 @@
+# write a program to format the following letter using escape sequence charaiters.
+
+letter = "Dear Hiren,\n\tThis Python Course is rice. \nTranks!"
+
+print(letter)
