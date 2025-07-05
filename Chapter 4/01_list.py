f7d287dc37e36ca4ead146cf35a68b9dc1aa288c
hiren223 <123714322+hiren223@users.noreply.github.com>
2025-07-05 10:38:37 +0530

    Done

---
+++ Chapter 4/01_list.py
@@ -0,0 +1,6 @@
+list = ["apple", "banana", "cherry", 5, 345.06, False, "Rohit", "Abhi"]
+
+print(list[0])
+list[0] = "orange" # Unlike Strings lists are mutable
+print(list[0])
+print(list[1:4]) 
---
+++ Chapter 4/02_list_method.py
@@ -0,0 +1,8 @@
+list = ["apple", "banana", "cherry", 5, 345.06, False, "Rohit", "Abhi"]
+print(list)
+list.append("Hiren") # Adds an item to the end of the list
+print(list)
+
+l1 = [2, 1, 4, 3, 5 , 6]
+l1.sort() # Sorts the list
+print(l1)
